b557efe12b419b6efc8c8b861ac1b2fec917ee5f
Andres Diez <andres.diez@redspace.com>
2016-02-01 14:04:54 -0400

    master commit

--- app/src/main/java/com/example/adiez/anytest/Asynk.java
+++
@@ -1,7 +0,0 @@
-package com.example.adiez.anytest;
-
-/**
- * Created by adiez on 2016-01-29.
- */
-public class Asynk {
-}
--- app/src/main/java/com/example/adiez/anytest/Textdtd.java
+++
@@ -1,8 +0,0 @@
-package com.example.adiez.anytest;
-
-/**
- * Created by adiez on 2016-01-30.
- */
-public @interface Textdtd {
-    String info() default "";
-}
